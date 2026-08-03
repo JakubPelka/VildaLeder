ffa5bb5b5da414ed646fbe01414f4613361d60a5
Lynx solutions <jakubpelka@gmail.com>
2026-08-03 15:31:11 +0200

    feat: invite community observation reporting

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ import {
   resolveSpecies,
   speciesCatalog,
   speciesLabel,
-} from "./core.js?v=20260803-trail-provenance-v10";
-import { translations, translator } from "./i18n.js?v=20260803-trail-provenance-v10";
+} from "./core.js?v=20260803-community-welcome-v11";
+import { translations, translator } from "./i18n.js?v=20260803-community-welcome-v11";
 import {
   clearUserLocation,
   fitAllTrails,
@@ -24,7 +24,7 @@ import {
   setUserLocation,
   showFeaturePopup,
   showObservationPopup,
-} from "./map.js?v=20260803-trail-provenance-v10";
+} from "./map.js?v=20260803-community-welcome-v11";
 
 const MAX_TAXA_SHOWN = 100;
 const OBSERVATION_TABLE_PAGE_SIZE = 100;
